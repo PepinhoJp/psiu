26bd7487250952a3c8642f6da3d2fb0f012f413e
PepinhoJp <joao.cunha@aluno.puc-rio.br>
2022-09-12 10:05:44 -0300

    Update models.py

--- psiu/models.py
+++ psiu/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+  id = models.AutoField(primary_key=True)
+  nome = models.CharFilent(max_length=100)
+  email = models.EmailField("Favor escrever um email valido", "Invalido")
+  celular = models.CharFilent(max_length=13)  
